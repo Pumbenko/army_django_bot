014c79f397824bf594cd28eb6fc9ca49a77a964e
Pumbenko <starodubetstv@gmail.com>
2022-07-27 21:31:42 +0200

    Allowed hosts added

--- bots/telegram_bot/telegram_bot.py
+++ bots/telegram_bot/telegram_bot.py
@@ -39,12 +39,17 @@ class TelegramBot():
                                                   auth_token='4f88462e4fb2aba1-1e7d4b58ff9485ae-2cda7320cb96f082',)
 
         # self.bot.send_message(chat_id=self.channel_name, text=update.message.text, parse_mode=telegram.ParseMode.HTML)
-        # files={'photo':open(f'media/{file.file_unique_id}', 'rb')}
+
+        file = self.bot.getFile(update.message.photo[-1].file_id)
+        obj = context.bot.get_file(file)
+        obj_url=f'static/media/imgs/{file.file_unique_id}.jpg'
+        obj.download(obj_url)
+        files={'photo':open(f'https://django-viber-telegram-bot.herokuapp.com/media/imgs/{file.file_unique_id}.jpg', 'rb')}
         a=5
         msg_text=update.message.text if update.message else ' '
 
 
-        # requests.post(f'https://api.telegram.org/bot{self.bot.token}/sendPhoto?chat_id=@Test_army&caption={msg_text}', files=files)
+        requests.post(f'https://api.telegram.org/bot{self.bot.token}/sendPhoto?chat_id=@Test_army&caption={msg_text}', files=files)
 
         a=5
         # self.bot.send_photo(chat_id=self.channel_name,
@@ -54,7 +59,7 @@ class TelegramBot():
 
         # viber_handler.send_text_message(msg_text)
 
-        viber_handler.send_picture(msg_text, f'https://viber-bot-rukoszop.herokuapp.com/{obj_url}')
+        viber_handler.send_picture(msg_text, f'https://django-viber-telegram-bot.herokuapp.com/media/imgs/{file.file_unique_id}.jpg')
         # viber_handler.send_picture(msg_text, update.channel_post.link)
 
         # update.message.reply_text(f'Your message has been successfully sent to desired channel! {update.message.text}')
