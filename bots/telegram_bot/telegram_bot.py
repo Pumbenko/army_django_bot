f291d2cb3d1ea59f4a71e90cbdfeb876b96f5825
Pumbenko <starodubetstv@gmail.com>
2022-08-03 20:11:26 +0200

    Allowed hosts added

--- bots/telegram_bot/telegram_bot.py
+++ bots/telegram_bot/telegram_bot.py
@@ -31,11 +31,11 @@ class TelegramBot():
         file = self.bot.getFile(update.message.photo[-1])
         obj = context.bot.get_file(file)
         # obj_url = f'imgs/{file.file_unique_id}.jpg'
-        obj_url = f'imgs/temp_.jpg'
+        obj_url = f'imgs/{file.file_unique_id}.jpg'
         obj.download(obj_url)
 
-        # photo_to_send=f'https://django-viber-telegram-bot.herokuapp.com/media/imgs/{file.file_unique_id}.jpg'
-        photo_to_send='https://django-viber-telegram-bot.herokuapp.com/media/imgs/temp_.jpg'
+        photo_to_send=f'https://django-viber-telegram-bot.herokuapp.com/media/{obj_url}'
+        # photo_to_send='https://django-viber-telegram-bot.herokuapp.com/media/imgs/temp_.jpg'
         msg_text = update.message.text if update.message.text else ''
 
         a=5
@@ -47,7 +47,7 @@ class TelegramBot():
         self.viber_handler.send_picture(msg_text,
                                    photo_to_send)
 
-        # os.remove(obj_url)
+        os.remove(obj_url)
 
 
     def pass_message(self, update: Update, context: CallbackContext):
